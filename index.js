57cf6af57138a078cc452ca4ce78c4e3453d3bd6
abhayshah1 <net_abhay@yahoo.com>
2021-02-07 23:01:32 -0500

    Send email

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const axios = require( 'axios' );
-const cheerio = require ( 'cheerio' );
+const nodemailer = require( 'nodemailer' );
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'abhayshah287@gmail.com',
+    pass: 'googleHome287'
+  }
+});
+
+
+const INCLUDE_MATCH = 0;
+const EXCLUDE_MATCH = 1;
 
 // shoprite
 const shopriteURL = 'https://vaccines.shoprite.com/';
 const matchString = "Vaccine appointment schedule is FULL";
-const matchType = 1; // 0 = vaccine available on match, 1 = vaccine not available on match
+const matchType = INCLUDE_MATCH; // 0 = vaccine available on match, 1 = vaccine not available on match
 
-axios.get(shopriteURL)
+var vaccineURL = shopriteURL;
+
+axios.get( vaccineURL )
   .then(response => {
-    //console.log(response.data);
-    if ( response.data.includes( matchString ) && matchType ) {
-        console.log("Shoprite No Vaccinnes");
+    if ( ( response.data.includes( matchString ) && matchType===INCLUDE_MATCH ) || 
+        ( !response.data.includes( matchString ) && matchType===EXCLUDE_MATCH ) ){
+          console.log( "Vaccine Found: " + vaccineURL );
+          var mailOptions = {
+            from: 'abhayshah287@gmail.com',
+            to: 'net_abhay@yahoo.com',
+            subject: 'Vaccine Availability Alert',
+            text: 'Vaccine Found' + vaccineURL
+          };
+          
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+    }
+    else {
+      console.log( "Vaccine Not Found: " + vaccineURL );
     }
   })
   .catch(error => {
